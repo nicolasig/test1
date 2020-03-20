67f8704edccdede6d47699230bd7fa20f68ee655
Nicolas <nicolasig@hotmail.com>
2020-03-20 11:21:23 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { AnimalFormComponent } from './components/animal-form/animal-form.component';
+import { AnimalListComponent } from './components/animal-list/animal-list.component';
+import { AnimalsService } from './services/animals.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AnimalFormComponent,
+    AnimalListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AnimalsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
